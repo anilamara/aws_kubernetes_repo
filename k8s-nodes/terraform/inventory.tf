9df6dd8f6764f2ea4a72131209bd4e63873221de
anilamara <anil.amara51@gmail.com>
2026-01-03 00:50:31 +0000

    update inventory.tf

--- k8s-nodes/terraform/inventory.tf
+++ k8s-nodes/terraform/inventory.tf
@@ -1,9 +1,14 @@
 resource "local_file" "ansible_inventory" {
+  filename = "/home/ubuntu/aws_kubernetes_repo/ansible/inventory/hosts.ini"
+
   content = templatefile("${path.module}/inventory.tpl", {
     control_ip = aws_instance.control_plane.private_ip
     worker_ips = aws_instance.workers[*].private_ip
   })
 
-  filename = "${path.module}/../../ansible/inventory/hosts.ini"
+  depends_on = [
+    aws_instance.control_plane,
+    aws_instance.workers
+  ]
 }
 
